76c54e6f078142ccceb9219b7a59f392d6e57410
yoyo200181 <113662537+yoyo200181@users.noreply.github.com>
2023-06-01 18:18:52 +0800

    up

---
+++ js/food.js
@@ -0,0 +1,562 @@
+function getFood() {
+    return [
+        {
+            type:"蔬菜",
+            Ingredients:[
+                {
+                    id:1,
+                    weight:0.5,
+                    ingre:[
+                        {
+                            index:1,
+                            name:"紅蘑菇"
+                        },
+                        {
+                            index:2,
+                            name:"綠蘑菇"
+                        },
+                        {
+                            index:3,
+                            name:"藍蘑菇"
+                        },
+                        {
+                            index:4,
+                            name:"月亮蘑菇"
+                        },
+                        {
+                            index:5,
+                            name:"仙人掌花"
+                        },
+                        {
+                            index:6,
+                            name:"海帶"
+                        }
+
+                    ],
+                    ingre_re:[]
+                    
+                },
+                {
+                    id:2,
+                    weight:1,
+                    ingre:[
+                        {
+                            index:5,
+                            name:"胡蘿蔔"
+                        },
+                        {
+                            index:6,
+                            name:"番茄"
+                        },
+                        {
+                            index:7,
+                            name:"土豆"
+                        },
+                        {
+                            index:8,
+                            name:"洋蔥"
+                        },
+                        {
+                            index:9,
+                            name:"大蒜"
+                        },
+                        {
+                            index:10,
+                            name:"辣椒"
+                        },
+                        {
+                            index:11,
+                            name:"蘆筍"
+                        },
+                        {
+                            index:12,
+                            name:"南瓜"
+                        },
+                        {
+                            index:13,
+                            name:"玉米"
+                        },
+                        {
+                            index:14,
+                            name:"仙人掌"
+                        },
+                        {
+                            index:15,
+                            name:"曼德拉草"
+                        },
+                        {
+                            index:16,
+                            name:"茄子"
+                        },
+                        {
+                            index:16,
+                            name:"苔蘚"
+                        },
+                        {
+                            index:16,
+                            name:"石果"
+                        },
+                        {
+                            index:16,
+                            name:"爆米花魚"
+                        },
+                        {
+                            index:16,
+                            name:"玉米鱈魚"
+                        },
+
+                    ],
+                    ingre_re:[]
+                }
+            ]
+        },
+
+        {
+            type:"水果",
+            Ingredients:[
+                {
+                    id:3,
+                    weight:0.5,
+                    ingre:[
+                        {
+                            index:15,
+                            name:"漿果"
+                        },
+                        {
+                            index:16,
+                            name:"多汁漿果"
+                        },
+                        {
+                            index:17,
+                            name:"小發光漿果"
+                        },
+                        {
+                            index:18,
+                            name:"無花果"
+                        }
+
+                    ],
+                    ingre_re:[]
+                },
+                {
+                    id:4,
+                    weight:1,
+                    ingre:[
+                        {
+                            index:19,
+                            name:"石榴"
+                        },
+                        {
+                            index:20,
+                            name:"西瓜"
+                        },
+                        {
+                            index:21,
+                            name:"火龍果"
+                        },
+                        {
+                            index:22,
+                            name:"香蕉"
+                        },
+                        {
+                            index:23,
+                            name:"大發光漿果"
+                        },
+                        {
+                            index:23,
+                            name:"榴槤"
+                        }
+                    ],
+                    ingre_re:[]
+                }
+            ]
+        },
+
+        {
+            type:"肉",
+            Ingredients:[
+                {
+                    id:5,
+                    weight:0.25,
+                    ingre:[],
+                    ingre_re:[
+                        {
+                            index:24,
+                            name:"藤壺"
+                        }
+                     
+                    ]
+
+                },
+                {
+                    id:5,
+                    weight:0.5,
+                    ingre:[
+                        {
+                            index:24,
+                            name:"小肉"
+                        },
+                        {
+                            index:25,
+                            name:"蛙腿"
+                        },
+                        {
+                            index:26,
+                            name:"雞腿"
+                        },
+                        {
+                            index:27,
+                            name:"蝙蝠翅膀"
+                        },
+                        {
+                            index:28,
+                            name:"裸露鼻孔"
+                        }
+
+                    ],
+
+                    ingre_re:[
+                        
+                        {
+                            index:28,
+                            name:"淡水魚"
+                        },
+                        {
+                            index:28,
+                            name:"小魚塊"
+                        },
+                        {
+                            index:28,
+                            name:"鰻魚"
+                        },
+                        {
+                            index:28,
+                            name:"小孔雀魚"
+                        },
+                        {
+                            index:28,
+                            name:"針鼻噴墨魚"
+                        },
+                        {
+                            index:28,
+                            name:"小餌魚"
+                        },
+                        {
+                            index:28,
+                            name:"三文魚苗"
+                        },
+                        {
+                            index:28,
+                            name:"落葉比目魚"
+                        },
+                        {
+                            index:28,
+                            name:"花朵金槍魚"
+                        },
+                        {
+                            index:28,
+                            name:"熾熱太陽魚"
+                        },
+                        {
+                            index:28,
+                            name:"口水魚"
+                        },
+
+
+                    ]
+
+                },
+                {
+                    id:6,
+                    weight:1,
+                    ingre:[
+                        {
+                            index:29,
+                            name:"大肉"
+                        },
+                        {
+                            index:30,
+                            name:"怪物肉"
+                        },
+                        {
+                            index:31,
+                            name:"葉肉"
+                        },
+                        {
+                            index:32,
+                            name:"象鼻"
+                        },
+                        {
+                            index:34,
+                            name:"鳥腿"
+                        }
+                        
+                    ],
+                    ingre_re:[
+                        
+                        {
+                            index:33,
+                            name:"魚"
+                        }
+                        
+                    ]
+                }
+            ]
+        },
+
+        {
+            type:"海鮮",
+            Ingredients:[
+                {
+                    id:5,
+                    weight:0.25,
+                    ingre:[
+                        {
+                            index:24,
+                            name:"藤壺"
+                        }
+                    ],
+                    ingre_re:[]
+
+                },
+                {
+                    id:5,
+                    weight:0.5,
+                    ingre:[
+                        {
+                            index:28,
+                            name:"淡水魚"
+                        },
+                        {
+                            index:28,
+                            name:"小魚塊"
+                        },
+                        {
+                            index:28,
+                            name:"小孔雀魚"
+                        },
+                        {
+                            index:28,
+                            name:"針鼻噴墨魚"
+                        },
+                        {
+                            index:28,
+                            name:"小餌魚"
+                        },
+                        {
+                            index:28,
+                            name:"三文魚苗"
+                        },
+                        {
+                            index:28,
+                            name:"落葉比目魚"
+                        },
+                        {
+                            index:28,
+                            name:"花朵金槍魚"
+                        },
+                        {
+                            index:28,
+                            name:"熾熱太陽魚"
+                        },
+                        {
+                            index:28,
+                            name:"口水魚"
+                        }
+
+                    ],
+
+                    ingre_re:[]
+
+                },
+                {
+                    id:6,
+                    weight:1,
+                    ingre:[
+ 
+                        {
+                            index:33,
+                            name:"魚"
+                        },
+                        {
+                            index:34,
+                            name:"生魚肉"
+                        },
+                        {
+                            index:34,
+                            name:"生魚肉"
+                        },
+                        {
+                            index:28,
+                            name:"鰻魚"
+                        },
+                        {
+                            index:28,
+                            name:"龍蝦"
+                        },
+                        {
+                            index:28,
+                            name:"泥魚"
+                        },
+                        {
+                            index:28,
+                            name:"班魚"
+                        },
+                        {
+                            index:28,
+                            name:"浮誇獅子魚"
+                        },
+                        {
+                            index:28,
+                            name:"黑鯰魚"
+                        },
+                        {
+                            index:28,
+                            name:"金錦鯉"
+                        },
+                        {
+                            index:28,
+                            name:"冰鯛魚"
+                        },
+                        {
+                            index:28,
+                            name:"甜味魚"
+                        }
+                        
+                    ],
+                    ingre_re:[]
+                }
+            ]
+        },
+
+        {
+            type:"怪物",
+            Ingredients:[
+                
+                {
+                    id:6,
+                    weight:1,
+                    ingre:[],
+                    ingre_re:[
+ 
+                        {
+                            index:33,
+                            name:"怪物肉"
+                        },
+                        {
+                            index:34,
+                            name:"榴槤"
+                        }
+                        
+                    ]
+                    
+                }
+            ]
+        },
+
+        {
+            type:"蛋",
+            Ingredients:[
+                
+                {
+                    id:6,
+                    weight:1,
+                    ingre:[
+ 
+                        {
+                            index:33,
+                            name:"鳥蛋"
+                        }
+                        
+                    ],
+                    ingre_re:[]
+                    
+                },
+                {
+                    id:6,
+                    weight:4,
+                    ingre:[
+ 
+                        {
+                            index:33,
+                            name:"高腳鳥蛋"
+                        }
+                        
+                    ],
+                    ingre_re:[]
+                    
+                }
+            ]
+        },
+
+        {
+            type:"蜂蜜",
+            Ingredients:[
+                
+                {
+                    id:6,
+                    weight:1,
+                    ingre:[
+ 
+                        {
+                            index:33,
+                            name:"蜂蜜"
+                        },
+                        {
+                            index:33,
+                            name:"蜂巢"
+                        }
+                        
+                    ],
+                    ingre_re:[]
+                    
+                },
+                {
+                    id:6,
+                    weight:3,
+                    ingre:[
+ 
+                        {
+                            index:33,
+                            name:"蜂王漿"
+                        }
+                        
+                    ],
+                    ingre_re:[]
+                    
+                }
+            ]
+        },
+
+        {
+            type:"奶製品",
+            Ingredients:[
+                
+                {
+                    id:6,
+                    weight:1,
+                    ingre:[
+ 
+                        {
+                            index:33,
+                            name:"黃油"
+                        },
+                        {
+                            index:33,
+                            name:"電羊奶"
+                        },
+                        {
+                            index:33,
+                            name:"乳白物"
+                        }
+                        
+                    ],
+                    ingre_re:[]
+                    
+                }
+            ]
+        },
+        
+
+    ]
+}
---
+++ js/recipesample.js
@@ -0,0 +1,74 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"Now Showing Movie",
+            thumbnail:"images/barbecue.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+            name:"Barbecue",
+            cast:"Darius Glenn, Maurice Powers",
+            director:"Aurora Raymond",
+            duration: 122
+        },
+        {
+            id:2,
+            type:"Now Showing Movie",
+            thumbnail:"images/bigbuck.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"Bigbuck",
+            cast:"Madelyn Guthrie, Anthony Mercer",
+            director:" Helen Waller",
+            duration: 86
+        },
+        {
+            id:3,
+            type:"Now Showing Movie",
+            thumbnail:"images/casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Cassblanca",
+            cast:"Humphrey Bogart, Ingrid Bergmen",
+            director:"Michael Curtiz",
+            duration: 124
+        }, {
+            id:4,
+            type:"Now Showing Movie",
+            thumbnail:"images/chocolate.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name:"Chocolate",
+            cast:"Stanley Lee, Archer Bird",
+            director:"Rex Hatfield",
+            duration: 82
+        },
+        {
+            id:5,
+            type:"Upcoming Movies",
+            thumbnail:"images/nature.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Nature",
+            cast:"Jaxx Christensen, Alexia Wells",
+            director:"Harry Kirkland",
+            duration: 99
+        },
+        {
+            id:6,
+            type:"Upcoming Movies",
+            thumbnail:"images/wildlife.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Wildlife",
+            cast:"Cali Gonzalez, Jesse Hutchinson",
+            director:"Harvey Rivera",
+            duration: 119
+        },
+        {
+            id:7,
+            type:"Upcoming Movies",
+            thumbnail:"images/wonders.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name:"Wonders",
+            cast:"Stanley Lee, Oakley Ward",
+            director:"Gregory Marshall",
+            duration: 110
+        }
+    ]
+}
+
